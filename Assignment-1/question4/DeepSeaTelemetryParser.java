44946e802b4fce537bea548f932704c19d889e7a
Yash Raturi <yashraturi70@gmail.com>
2026-06-04 10:41:09 +0530

    Q4

--- Assignment-1/question4/DeepSeaTelemetryParser.java
+++ Assignment-1/question4/DeepSeaTelemetryParser.java
@@ -11,41 +11,14 @@ class SensorCorruptionException extends RuntimeException {
 }
 
 class TelemetryStream implements AutoCloseable {
-    public void readData() throws HardwareLockException {
-        boolean fileLocked = true; 
-        if (fileLocked) {
-            throw new HardwareLockException("OS file lock detected.");
-        }
-    }
-
-    public void processSensorData(int temperature) {
-        if (temperature > 100) {
-            throw new SensorCorruptionException("Impossible sensor values detected.");
-        }
-    }
-
     @Override
-    public void close() {
-        System.out.println("TelemetryStream closed automatically.");
-    }
+    public void close() {}
 }
 
 public class DeepSeaTelemetryParser {
     public void parseTelemetry() throws HardwareLockException {
         try (TelemetryStream stream = new TelemetryStream()) {
-            stream.readData();
-            stream.processSensorData(500);
-        } catch (SensorCorruptionException e) {
-            System.out.println("Logged recoverable error: " + e.getMessage());
-        }
-    }
-
-    public static void main(String[] args) {
-        DeepSeaTelemetryParser parser = new DeepSeaTelemetryParser();
-        try {
-            parser.parseTelemetry();
-        } catch (HardwareLockException e) {
-            System.err.println("Process crashed due to fatal hardware failure: " + e.getMessage());
+            
         }
     }
 }
